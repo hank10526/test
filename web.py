4c608b0fd4d40a70a7d4138488355a08e9c73677
hank10526 <hank10526@gmail.com>
2026-05-10 05:04:13 +0800

    Update web.py

--- web.py
+++ web.py
@@ -106,13 +106,202 @@ def movie2():
                 "showLength": runtime_text[13:].strip(),
                 "lastUpdate": lastUpdate
             }
-            db.collection("電影2A").document(movie_id).set(doc)
+            db.collection("靜宜資管").document(movie_id).set(doc)
             
         return f"爬蟲完成！最後更新日期：{lastUpdate}"
     except Exception as e:
         return f"爬蟲發生錯誤: {e}"
 
 # ... 其餘 math, search, read 路由維持邏輯不變 ...
+@app.route("/movie1")
+def movie1():
+    R = ""
+    url = "https://www.atmovies.com.tw/movie/next/"
+    Data = requests.get(url)
+    Data.encoding = "utf-8"
+    #print(Data.text)
+    sp = BeautifulSoup(Data.text, "html.parser")
+    result=sp.select(".filmListAllX li")
+    for item in result:
+        introduce = "https://www.atmovies.com.tw" + item.find("a").get("href")
+        R +=  "<a href=" + introduce + ">" + item.find("img").get("alt") + "</a><br>"
+        post = "https://www.atmovies.com.tw" + item.find("img").get("src")
+        R += "<img src=" + post + "> </img><br><br>" 
+    return R    
+
+@app.route("/spider1")
+def spider1():
+    R = ""
+    url = "https://www1.pu.edu.tw/~tcyang/course.html"
+    Data = requests.get(url)
+    Data.encoding = "utf-8"
+    sp = BeautifulSoup(Data.text, "html.parser")
+    result=sp.select(".team-box a")
+
+    for i in result:
+        R += i.text + i.get("href") + "<br>" 
+    return R
+
+@app.route("/search", methods=["GET", "POST"])
+def search():
+    db = firestore.client()
+    results = []
+    keyword = ""
+    
+    if request.method == "POST":
+        keyword = request.form.get("keyword")
+        collection_ref = db.collection("靜宜資管2026a")
+        docs = collection_ref.get()
+
+        for doc in docs:
+            user = doc.to_dict()
+            if keyword in user["name"]:
+                results.append({
+                    "name": user["name"],
+                    "lab": user["lab"]
+                })
+
+    return render_template("search.html", results=results, keyword=keyword)
+
+
+@app.route("/read2")
+def read2():
+    Result = ""
+    keyword = "楊"
+    db = firestore.client()
+    collection_ref = db.collection("靜宜資管2026B")    
+    docs = collection_ref.get()
+    for doc in docs: 
+        teacher = doc.to_dict()
+        if keyword in teacher["name"]:        
+            Result += str(teacher) + "<br>"
+
+    if Result == "":
+        Result = "抱歉,查無此關鍵字姓名之老師資料"    
+    return Result
+
+@app.route("/read")
+def read():
+    Result = ""
+    db = firestore.client()
+    collection_ref = db.collection("靜宜資管2026B")    
+    docs = collection_ref.get()
+    docs = collection_ref.order_by("lab", direction=firestore.Query.DESCENDING).get()
+    for doc in docs:         
+        Result += str(doc.to_dict()) + "<br>"    
+    return Result
+
+@app.route("/mis")
+def course():
+    return "<h1>資訊管理導論</h1><a href=/>返回首頁</a>"
+
+@app.route("/today")
+def today():
+    now = datetime.now()
+    return render_template("today.html", datetime=str(now))
+
+@app.route("/about")
+def about():
+    return render_template("mis2a.html")
+
+@app.route("/welcome", methods=["GET"])
+def welcome():
+    user = request.values.get("u")
+    d = request.values.get("d")
+    c = request.values.get("c")    
+    return render_template("welcome.html", name= user, dep = d, course = c)
+
+
+@app.route("/account", methods=["GET", "POST"])
+def account():
+    if request.method == "POST":
+        user = request.form["user"]
+        pwd = request.form["pwd"]
+        result = "您輸入的帳號是：" + user + "; 密碼為：" + pwd 
+        return result
+    else:
+        return render_template("account.html")
+
+
+@app.route("/math", methods=["GET", "POST"])
+def math():
+    if request.method == "POST":
+        x = int(request.form["x"])
+        opt = request.form["opt"]
+        y = int(request.form["y"])      
+        result = "您輸入的是：" + str(x) + opt + str(y)
+        
+        if (opt == "/" and y == 0):
+            result += "，除數不能為0"
+        else:
+            match opt:
+                case "+":
+                    r = x + y
+                case "-":
+                    r = x - y
+                case "*":
+                    r = x * y
+                case "/":
+                    r = x / y  # 修正：之前誤寫為 x - y
+                case _:
+                    return "未知運算符號"
+            result += "=" + str(r)  + "<br><a href=/>返回首頁</a>"          
+        return result
+    else:
+        return render_template("math.html")
+
+@app.route('/cup', methods=["GET"])
+def cup():
+    # 檢查網址是否有 ?action=toss
+    #action = request.args.get('action')
+    action = request.values.get("action")
+    result = None
+    
+    if action == 'toss':
+        # 0 代表陽面，1 代表陰面
+        x1 = random.randint(0, 1)
+        x2 = random.randint(0, 1)
+        
+        # 判斷結果文字
+        if x1 != x2:
+            msg = "聖筊：表示神明允許、同意，或行事會順利。"
+        elif x1 == 0:
+            msg = "笑筊：表示神明一笑、不解，或者考慮中，行事狀況不明。"
+        else:
+            msg = "陰筊：表示神明否定、憤怒，或者不宜行事。"
+            
+        result = {
+            "cup1": "/static/" + str(x1) + ".jpg",
+            "cup2": "/static/" + str(x2) + ".jpg",
+            "message": msg
+        }
+        
+    return render_template('cup.html', result=result)
+
+
+
+@app.route("/math2", methods=["GET", "POST"])
+def math2():
+    result = None
+    if request.method == "POST":
+        # 取得使用者輸入
+        x = int(request.form.get("x"))
+        opt = request.form.get("opt")
+        y = int(request.form.get("y"))
+
+        # 你的核心邏輯
+        match opt:
+            case "∧":
+                result = x ** y
+            case "√":
+                if y != 0:
+                    result = x ** (1/y)
+                else:
+                    result = "數學上不存在「0 次方根」"
+            case _:
+                result = "請輸入∧(次方)或√(根號)"
+    return render_template("math2.html", result=result)
+
 
 @app.route("/today")
 def today():
